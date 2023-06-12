0bbd17187f6740885149213813978232c1946ef4
Priya v <priyavel7358@gmail.com>
2023-06-12 12:24:56 +0530

    commit changes2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'app2',
+        filename: 'static/chunks/remoteEntry.js',
+        shared: {
+          // whatever else
+        },
+        extraOptions: {
+          exposePages: true
+        }
+      })
+    );
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
